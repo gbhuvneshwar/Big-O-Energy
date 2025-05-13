512bcb5b6ffd31d3a3fcba2442ff9a2f5258b67a
gbhuvneshwar <b.chouksey27@protonmail.com>
2025-05-13 20:05:12 +0530

    string prob

---
+++ 75/arr_str/reverse_words_ in_string.py
@@ -0,0 +1,57 @@
+
+"""
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ 
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+
+Seen this question in a real interview before?
+1/5
+"""
+def reverseWords(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    s_l = s.split()
+    s_l = s_l[::-1]
+    op = " ".join(s_l)
+    return op
+
+
+#Test cases
+assert reverseWords("a good   example") == "example good a"
+assert reverseWords("the sky is blue") == "blue is sky the"
+assert reverseWords("  hello world  ") == "world hello"
+assert reverseWords("a good   example") == "example good a"
+assert reverseWords("the sky is blue") == "blue is sky the"
